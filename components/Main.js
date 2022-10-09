f1f95cf6b38918c069e3764dcfd5f166da8d6369
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2022-10-09 23:18:54 +1100

    add Selector component in slideshow

--- components/Main.js
+++ components/Main.js
@@ -8,7 +8,7 @@ export default function Main() {
     "https://d33wubrfki0l68.cloudfront.net/49de349d12db851952c5556f3c637ca772745316/cfc56/static/images/wallpapers/bridge-02@2x.png",
     "https://d33wubrfki0l68.cloudfront.net/594de66469079c21fc54c14db0591305a1198dd6/3f4b1/static/images/wallpapers/bridge-01@2x.png"
   ];
-  const images= ["/images/main.jpg","/images/access.jpg","/images/fireworks.jpg"]
+  const images= ["/images/main.jpg","/images/access.jpg","/images/fireworks.jpg","https://d33wubrfki0l68.cloudfront.net/dd23708ebc4053551bb33e18b7174e73b6e1710b/dea24/static/images/wallpapers/shared-colors@2x.png",]
   return (
     
     < Flex
--- components/images/Slideshow.js
+++ components/images/Slideshow.js
@@ -1,16 +1,50 @@
-import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Text, Heading, Center } from "@chakra-ui/react";
 // import Image from "next/image";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { wrap } from "popmotion";
 
-
-export default function Slideshow({images}) {
-  // const imagesArray = [
-  //   "https://d33wubrfki0l68.cloudfront.net/dd23708ebc4053551bb33e18b7174e73b6e1710b/dea24/static/images/wallpapers/shared-colors@2x.png",
-  //   "https://d33wubrfki0l68.cloudfront.net/49de349d12db851952c5556f3c637ca772745316/cfc56/static/images/wallpapers/bridge-02@2x.png",
-  //   "https://d33wubrfki0l68.cloudfront.net/594de66469079c21fc54c14db0591305a1198dd6/3f4b1/static/images/wallpapers/bridge-01@2x.png"
-  // ];
+export default function Slideshow({ images }) {
+  if (typeof images[0] !== "object") {
+    images = images.map((image) => {
+      return { url: image };
+    });
+  }
+  function Selector() {
+    const selectorWidth = images.length * 32
+    return (
+      <Flex
+        w="100%"
+        position={"absolute"}
+        justifyContent="center"
+        bottom="20px"
+        zIndex={"10"}
+      >
+        <Flex fontWeight={"bold"} w={selectorWidth} justifyContent="center">
+          {images.map((val, index) => {
+            return (
+              <Box
+                fontWeight={"bold"}
+                color={"gray"}
+                transition=".3s"
+                key={index}
+                m="0 0.5rem"
+                onClick={() =>
+                  imageIndex !== index && paginate(index - imageIndex)
+                }
+              >
+                <Center>
+                  <Box fontSize={{base:"0.5rem", md:"1rem"}} _hover={{ color: "white" }}>
+                    {index === imageIndex ? `○` : `●`}
+                  </Box>
+                </Center>
+              </Box>
+            );
+          })}
+        </Flex>
+      </Flex>
+    );
+  }
   const variants = {
     enter: (direction) => {
       return {
@@ -19,22 +53,21 @@ export default function Slideshow({images}) {
       };
     },
     center: {
-
       zIndex: 1,
       x: 0,
       opacity: 1,
-      transition:{
-        duration:0.5
-      }
+      transition: {
+        duration: 0.5,
+      },
     },
     exit: (direction) => {
       return {
         zIndex: 0,
         x: direction < 0 ? 1000 : -1000,
         opacity: 0,
-        transition:{
-          duration:0.5
-        }
+        transition: {
+          duration: 0.5,
+        },
       };
     },
   };
@@ -50,49 +83,71 @@ export default function Slideshow({images}) {
   // absolute page index as the `motion` component's `key` prop, `AnimatePresence` will
   // detect it as an entirely new image. So you can infinitely paginate as few as 1 images.
   const imageIndex = wrap(0, images.length, page);
-  console.log("IMI",imageIndex, direction, page)
   const paginate = (newDirection) => {
     setPage([page + newDirection, newDirection]);
   };
   return (
-    <Flex position="relative" justifyContent={"center"} alignItems="center" w="100%" h="100%">
-      <AnimatePresence initial={false} custom={direction}>
-        <Image
-          as={motion.img}
+    <Box w="100%" h="100%" position="relative">
+      <Flex
+        as={AnimatePresence}
+        initial={false}
+        custom={direction}
+        position="relative"
+        justifyContent={"center"}
+        // alignItems="center"
+        w="100%"
+        h="100%"
+      >
+        <Box
+          as={motion.div}
           key={page}
-          src={images[imageIndex]}
+          w="100%"
+          h="100%"
+          position="absolute"
           custom={direction}
           variants={variants}
           initial="enter"
           animate="center"
           exit="exit"
-          h={"100%"}
-          w="100%"
-          position="absolute"
-          layout="fill"
-          objectFit="cover"
-          objectPosition="50% 0"
           transition={{
             x: { type: "spring", stiffness: 300, damping: 30 },
             opacity: { duration: 0.2 },
           }}
-          drag="x"
-          dragConstraints={{ left: 0, right: 0 }}
-          dragElastic={1}
-          onDragEnd={(e, { offset, velocity }) => {
-            const swipe = swipePower(offset.x, velocity.x);
+        >
+          <Image
+            as={motion.img}
+            src={images[imageIndex].url}
+            h={"100%"}
+            w="100%"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="50% 0"
+            drag="x"
+            dragConstraints={{ left: 0, right: 0 }}
+            dragElastic={1}
+            onDragEnd={(e, { offset, velocity }) => {
+              const swipe = swipePower(offset.x, velocity.x);
 
-            if (swipe < -swipeConfidenceThreshold) {
-              paginate(1);
-            } else if (swipe > swipeConfidenceThreshold) {
-              paginate(-1);
-            }
-          }}
-        />
-      </AnimatePresence>
+              if (swipe < -swipeConfidenceThreshold) {
+                paginate(1);
+              } else if (swipe > swipeConfidenceThreshold) {
+                paginate(-1);
+              }
+            }}
+          />
+          {Object.keys(images[imageIndex]).some((val) => val === "text") && (
+            <Heading
+            //will set style
+            >
+              {val.text}
+            </Heading>
+          )}
+        </Box>
+      </Flex>
+      {/* </AnimatePresence> */}
       <Flex
-        // top="calc(50% - 20px)"
-        left='10px'
+        top="calc(50% - 20px)"
+        left="10px"
         position="absolute"
         w="30px"
         h="40px"
@@ -106,14 +161,12 @@ export default function Slideshow({images}) {
         borderRadius={"0.2rem"}
         transform="scale(-1)"
         transition={".3s"}
-        _hover={{bg:"rgba(0,0,0,.7)"}}
+        _hover={{ bg: "rgba(0,0,0,.7)" }}
         onClick={() => paginate(-1)}
       >
-        <Box display="inline-block"> 
-        {"❯"}
-        </Box>
+        <Box display="inline-block">{"❯"}</Box>
       </Flex>
-      <Flex 
+      <Flex
         top="calc(50% - 20px)"
         position="absolute"
         w="30px"
@@ -128,10 +181,12 @@ export default function Slideshow({images}) {
         borderRadius={"0.2rem"}
         fontSize={"16px"}
         transition={".3s"}
-        _hover={{bg:"rgba(0,0,0,.7)"}}
-        onClick={() => paginate(1)}>
+        _hover={{ bg: "rgba(0,0,0,.7)" }}
+        onClick={() => paginate(1)}
+      >
         {"❯"}
       </Flex>
-    </Flex>
+      <Selector />
+    </Box>
   );
 }
